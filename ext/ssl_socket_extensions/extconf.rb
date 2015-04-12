c79bd08525c62f8c98c90c812be5efa1adb235a3
Charlie Sharpsteen <source@sharpsteen.net>
2015-04-11 23:04:34 -0700

    Use the OpenSSL headers that Ruby was built with
    Using headers from a different version of OpenSSL is a sure way to create
    segfaults when we start digging around in the SSL struct.

--- ext/ssl_socket_extensions/extconf.rb
+++ ext/ssl_socket_extensions/extconf.rb
@@ -1,3 +1,17 @@
 require 'mkmf'
+require 'shellwords'
 
+openssl_include = []
+openssl_lib     = []
+
+# Check to see if Ruby was compiled with a custom OpenSSL
+openssl_config = CONFIG['configure_args'].shellsplit.find {|e| e.start_with? '--with-openssl-dir='}
+unless openssl_config.nil?
+  _, _, openssl_dir = openssl_config.partition('=')
+
+  openssl_include << File.join(openssl_dir, 'include')
+  openssl_lib     << File.join(openssl_dir, 'lib')
+end
+
+dir_config('openssl', openssl_include, openssl_lib)
 create_makefile('openssl/keylog/ssl_socket_extensions')
