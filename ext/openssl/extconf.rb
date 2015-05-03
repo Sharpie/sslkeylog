2f2df025aee051c41d8674f369ec4e181a388d36
Charlie Sharpsteen <source@sharpsteen.net>
2015-05-03 15:02:10 -0700

    Fail early if openssl/ssl.h cannot be found
    If the OpenSSL headers cannot be found, compilation now aborts with a friendly
    error message.
    
    Closes GH-8.

--- ext/openssl/extconf.rb
+++ ext/openssl/extconf.rb
@@ -14,4 +14,12 @@ unless openssl_config.nil?
 end
 
 dir_config('openssl', openssl_include, openssl_lib)
+
+unless find_header('openssl/ssl.h')
+  abort <<-EOS
+Could not find openssl/ssl.h. Ensure the OpenSSL headers are installed.
+The --with-openssl-include option can be used to search a non-standard location.
+EOS
+end
+
 create_makefile('sslkeylog/openssl')
